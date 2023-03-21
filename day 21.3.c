ba4801548c41ad8f701a9a3daa1351b883b3e3f3
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-03-21 12:33:46 +0530

    Add files via upload

---
+++ day 21.3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main (){
+	
+	char a[] = "Hello";
+	
+	
+	printf("Enter value :");
+	scanf("%s",a);
+	
+    printf("string Lowercase : %s", strlwr(a));
+	  
+	  return 0;
+}
