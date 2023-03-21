527c99767772315e801652b0fb1843a631b3eae4
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-03-21 12:24:24 +0530

    Add files via upload

---
+++ day 21.5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main (){
+	
+	char a[50];
+	int i;
+	
+	printf("Enter value :");
+	scanf("%s",a);
+	
+	if (a[0]>='a' && a[0]<='z'){
+		
+		a[0]-=32;
+	}
+	
+	for (i=1;a[i]!='\0';i++){
+		
+		if (a[i]>='A' && a[i]<='Z'){
+			
+			a[i] +=32;
+		}
+	}
+	  
+	  printf("%s",a);
+	  
+	  return 0;
+}
