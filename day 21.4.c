90d8ae8bceb27f445fd0a47f520818df58cabde7
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-03-22 11:53:38 +0530

    Update day 21.4.c

--- day 21.4.c
+++ day 21.4.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
+#include<string.h>
 
 int main (){
 	
-	char a[] = "Hello";
+	char a[50];
 	
 	
 	printf("Enter value :");
-	scanf("%s",a);
+	gets(a);
 	
     printf("string uppercase : %s", strupr(a));
 	  
